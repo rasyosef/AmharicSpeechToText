48d8b799d0bebd5a4230c89d5a9d4d234798216f
Yosef Alemneh <yosefworku67@gmail.com>
2021-08-10 04:00:38 +0300

    added model training script

--- scripts/models.py
+++ scripts/models.py
@@ -74,3 +74,56 @@ def model_1(encoder):
     model.compile(optimizer=opt)
     
     return model
+
+def model_2(encoder):
+    input_img = layers.Input(
+        shape=(480, 640, 4), name="image", dtype="float32"
+    )
+    labels = layers.Input(name="label", shape=(None,), dtype="float32")
+
+    # First conv block
+    x = layers.Conv2D(
+        32,
+        (3, 3),
+        activation="relu",
+        kernel_initializer="he_normal",
+        padding="same",
+        name="Conv1",
+    )(input_img)
+    x = layers.MaxPooling2D((2, 2), name="pool1")(x)
+
+    # We have used two max pool with pool size and strides 2.
+    # Hence, downsampled feature maps are 4x smaller. The number of
+    # filters in the last layer is 64. Reshape accordingly before
+    # passing the output to the RNN part of the model
+    if tf.__version__ >= '2.3':
+        new_shape = (x.type_spec.shape[-3], x.type_spec.shape[-2]*x.type_spec.shape[-1])
+    else:
+        new_shape = (x.shape[-3], x.shape[-2]*x.shape[-1])
+    x = layers.Reshape(target_shape=new_shape, name="reshape")(x)
+    x = layers.Dense(64, activation="relu", name="dense1")(x)
+    x = layers.Dropout(0.2)(x)
+
+    # RNNs
+    x = layers.Bidirectional(layers.LSTM(128, return_sequences=True, dropout=0.25))(x)
+    x = layers.Bidirectional(layers.LSTM(64, return_sequences=True, dropout=0.25))(x)
+
+    # Output layer
+    x = layers.Dense(
+        len(encoder.classes_) + 1, activation="softmax", name="dense2"
+    )(x)
+
+    # Add CTC layer for calculating CTC loss at each step
+    output = CTCLayer(name="ctc_loss")(labels, x)
+
+    # Define the model
+    model = tf.keras.models.Model(
+        inputs=[input_img, labels], outputs=output, name="ocr_model_v1"
+    )
+    # Optimizer
+    opt = tf.keras.optimizers.Adam()
+    # Compile the model and return
+    model.compile(optimizer=opt)
+    
+    return model
+
